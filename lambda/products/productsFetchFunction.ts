5fa98b4587d848010c48874eca40ade00fbd7777
diegosantosouza <diegosantos.s@hotmail.com>
2022-09-17 23:22:24 -0300

    feat: products fetch functions implements repository resources

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,28 +1,45 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ProductRepository } from "/opt/nodejs/productsLayer";
+import { DynamoDB } from "aws-sdk";
+
+const productsDdb = process.env.PRODUCTS_DB!
+const ddbClient = new DynamoDB.DocumentClient()
+
+const productRepository = new ProductRepository(ddbClient, productsDdb)
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   const method = event.httpMethod;
   const lambdaRequestId = context.awsRequestId;
   const apiRequestId = event.requestContext.requestId;
   if (event.resource == '/products') {
-    if(method === 'GET') {
+    if (method === 'GET') {
       console.log('GET')
-      return { 
+
+      const products = await productRepository.getAllProducts();
+      return {
         statusCode: 200,
-        body: JSON.stringify({ 
-          message: 'GET Products - OK'
-        })
+        body: JSON.stringify(products)
       }
     }
   } else if (event.resource == '/products/{id}') {
     const productId = event.pathParameters!.id as string
     console.log(`GET /products/${productId}`)
-    return {
-      statusCode: 200,
-      body: `GET /products/${productId}`
+
+    try {
+      const products = await productRepository.getProductById(productId);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(products)
+      }
+    } catch (error) {
+      console.error((<Error>error).message)
+      return {
+        statusCode: 404,
+        body: (<Error>error).message
+      }
     }
   }
-  return { 
+  return {
     statusCode: 400,
     body: JSON.stringify({
       message: 'Bad Request'
