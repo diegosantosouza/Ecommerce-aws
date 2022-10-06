b98d31b82389d47e77d5ddd7150834bb14d4bf1f
diegosantosouza <diegosantos.s@hotmail.com>
2022-10-06 20:27:06 -0300

    feat: product event handler

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -5,11 +5,14 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 
+interface ProductsAppStackProps extends cdk.StackProps {
+  eventsDdb: dynamodb.Table
+}
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
   readonly productsAdminHandler: lambdaNodeJS.NodejsFunction
   readonly productsDb: dynamodb.Table
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: ProductsAppStackProps) {
     super(scope, id, props)
 
     this.productsDb = new dynamodb.Table(this, 'ProductsDb', {
@@ -27,6 +30,26 @@ export class ProductsAppStack extends cdk.Stack {
     // Products Layer
     const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, 'ProductsLayerVersionArn');
     const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'ProductsLayerVersionArn', productsLayerArn);
+
+    const productsEventsHandler = new lambdaNodeJS.NodejsFunction(this,
+      "ProductsEventsFunction", {
+      functionName: "ProductsFetchFunction",
+      entry: "lambda/products/productEventsFunction.ts",
+      handler: "handler",
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(2),
+      bundling: {
+        minify: true,
+        sourceMap: false,
+      },
+      environment: {
+        EVENTS_DB: props.eventsDdb.tableName
+      },
+      layers: [productsLayer],
+      tracing: lambda.Tracing.ACTIVE,
+      insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
+    })
+    props.eventsDdb.grantWriteData(productsEventsHandler);
 
     this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this,
       "ProductsFetchFunction", {
@@ -60,12 +83,14 @@ export class ProductsAppStack extends cdk.Stack {
         sourceMap: false,
       },
       environment: {
-        PRODUCTS_DB: this.productsDb.tableName
+        PRODUCTS_DB: this.productsDb.tableName,
+        PRODUCT_EVENT_FUNCTION_NAME: productsEventsHandler.functionName
       },
       layers: [productsLayer],
       tracing: lambda.Tracing.ACTIVE,
       insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
     })
     this.productsDb.grantWriteData(this.productsAdminHandler)
+    productsEventsHandler.grantInvoke(this.productsAdminHandler)
   }
 }
